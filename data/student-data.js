57476092c35e1924cd1deee82de7246f9e1a600c
Jenn <jenniferjuarezjj@gmail.com>
2022-09-20 21:40:14 -0400

    update student list

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Grade assignments', done: true, _id: 125223},
+  {text: 'Permission slips', done: false, _id: 127904},
+  {text: 'Math test assigned', done: false, _id: 139608},
+]
+
+export { 
+	students
+}
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // import modules
 
 import express from 'express'
-
+import { students } from './data/student-data.js'
 // Create Express app
 
 const app = express()
 
 // Configure the app (app.set)
 
-
+app.set('view engine', 'ejs')
 
 // Mount Middleware (app.use)
 
@@ -19,9 +19,17 @@ const app = express()
 app.get('/', function(req, res) {
   res.send('<h1>hello, friend</h1>')
 })
+
 app.get('/home', function(req, res) {
-  res.send('<h1>Home Page</h1>')
+  res.render('home')
 })
+
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students: students
+  })
+})
+
 
 // Tell the app to listen on port 3000
 
